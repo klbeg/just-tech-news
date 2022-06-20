d3916353e34ac787231346a6ef13a6b169041904
B33Gs <speschk@yahoo.com>
2022-06-20 17:37:24 -0400

    -setup basic server

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+const sequelize = new Sequelize('just_tech_news_db', 'root', 'SoupsSecure', {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306,
